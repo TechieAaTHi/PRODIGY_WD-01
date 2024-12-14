9dfc9227dcbd941cc97c56f83a0dec46383c79e0
Aathithiyan <154624026+TechieAaTHi@users.noreply.github.com>
2024-12-14 21:33:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function rentBike(bikeName) {
+    alert("You have selected the " + bikeName + " for rent!");
+}
+
+document.getElementById("rentNowBtn").addEventListener("click", function() {
+    alert("Welcome! Start exploring the bikes.");
+});
